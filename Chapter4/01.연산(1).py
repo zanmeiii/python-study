f63fc2489bbd0eb739c3841ecc9080e66fc77af3
zanmeiii <chanmi336@naver.com>
2022-09-23 11:17:28 +0900

    첫 커밋

---
+++ Chapter4/01.연산(1).py
@@ -0,0 +1,26 @@
+# 1. 대입연산
+# 변수이름 = 데이터
+
+# 2. 산술연산
+#  - 숫자연산
+x = 5
+y = 2
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+print(x ** y)
+
+# - 문자열 연산
+tag1 = "#내꺼하자"
+tag2 = "#오늘부터1일"
+tag3 = "#여친생김"
+
+tag = tag1 + tag2 + tag3
+print(tag)
+
+
+message = "우린 모두 파이썬을 사랑합니다" * 5
